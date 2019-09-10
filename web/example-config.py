bc97b0a6c89dcf2d2f0631bb540a9fff6534e0f7
Zach Lang <zach.d.lang@gmail.com>
2019-09-11 00:09:06 +1200

    Adding example config for CI testing

---
+++ web/example-config.py
@@ -0,0 +1,16 @@
+DBHOST = 'localhost'
+DBPORT = '5432'
+DBNAME = 'reader'
+DBUSER = 'postgres'
+DBPASS = 'password'
+
+CELERY_BACKEND = 'celerybackend'
+CELERY_BROKER = 'celerybroker'
+
+SENTRY_DSN = 'sentrydsn'
+CELERY_SENTRY_DSN = 'celerysentrydsn'
+
+TO_EMAIL = 'admin@email.com'
+FROM_EMAIL = 'no-reply@email.com'
+
+SECRETKEY = 'secretkey'
