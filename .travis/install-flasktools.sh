d7983bc49dec0f81218066c2bfd7bf90307dadaf
Zach Lang <zach.d.lang@gmail.com>
2019-12-29 20:15:46 +1300

    Changing flasktools to package version

--- .travis/install-flasktools.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-rm -rf ../flasktools
-git clone https://github.com/zachdlang/flasktools.git ../flasktools
--- web/example-config.py
+++ web/example-config.py
@@ -8,7 +8,6 @@ CELERY_BACKEND = 'celerybackend'
 CELERY_BROKER = 'celerybroker'
 
 SENTRY_DSN = 'sentrydsn'
-CELERY_SENTRY_DSN = 'celerysentrydsn'
 
 TO_EMAIL = 'admin@email.com'
 FROM_EMAIL = 'no-reply@email.com'
